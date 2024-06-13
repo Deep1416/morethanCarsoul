31dd4bc9f8a7ae87d8fd15381f2a3bab72868e22
Deependra <deependra186037@gamil.com>
2024-06-14 01:34:25 +0530

    almost complete

--- src/component/Carsoul.jsx
+++ src/component/Carsoul.jsx
@@ -18,6 +18,37 @@ const images = [
   "https://images.unsplash.com/photo-1718116088537-212b192d1ad9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
+const thoughts = [
+  {
+    shayari: "When I lost, I find you....",
+    des: "Do you have a map? Because I just keep getting lost in your eyes every time I see you. Your gaze pulls me in, leaving me breathless and enchanted....",
+  },
+  {
+    shayari: "In your eyes, I found my home....",
+    des: "If kisses were snowflakes, I'd send you a blizzard. Each kiss, a testament of my affection, would cover you, reminding you how deeply I cherish you....",
+  },
+  {
+    shayari: "Your smile is my sunshine....",
+    des: "I must be a snowflake, because I've fallen for you. Your warmth melts my heart, bringing joy and light into my life every single day....",
+  },
+  {
+    shayari: "Every moment with you is a treasure....",
+    des: "Are you a magician? Because whenever I look at you, everyone else disappears. Your presence is magical, making the world fade away as I focus on you....",
+  },
+  {
+    shayari: "Your love is my anchor....",
+    des: "If you were a vegetable, you'd be a cute-cumber. Your sweetness and charm captivate me, anchoring my heart in a sea of love and tenderness....",
+  },
+  {
+    shayari: "In your embrace, I find peace....",
+    des: "If you were a fruit, you'd be a fine-apple. Your embrace is a sanctuary where I find comfort and tranquility, rejuvenating my soul with your love....",
+  },
+  {
+    shayari: "Your presence is my comfort....",
+    des: "Do you have a Band-Aid? Because I just scraped my knee falling for you. Your presence heals my wounds, offering solace and warmth in times of need...",
+  },
+];
+
 const songs = [song1, song2, song3, song4, song5, song6];
 
 const Carousel = () => {
@@ -44,15 +75,23 @@ const Carousel = () => {
     }
   }, [startIndex, audioLoaded]);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setStartIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId); // Cleanup interval on unmount
+  }, []);
+
   const handleNext = () => {
     setStartIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
 
-  const handlePrev = () => {
-    setStartIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
+  // const handlePrev = () => {
+  //   setStartIndex((prevIndex) =>
+  //     prevIndex === 0 ? images.length - 1 : prevIndex - 1
+  //   );
+  // };
 
   const displayedImages = [
     images[startIndex],
@@ -70,29 +109,29 @@ const Carousel = () => {
           className="w-full h-full object-cover bgImages"
         />
       </div>
-      <div className="relative z-10 flex items-center justify-center gap-40">
-        <div className="w-64 h-80 border border-black bg-white p-4">
-            hello
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam
-          quas nobis voluptates aut iure quod sed explicabo distinctio
-          veritatis repellendus, dolore labore corporis, voluptate laborum
-          praesentium nam quae beatae optio deleniti! Expedita illum voluptas
-          animi sit ducimus quam porro.
+      <div className="relative z-10 flex items-center justify-center gap-32">
+        <div className="w-64 h-72  bg-transparent p-4 text-white bg-gray-400 rounded-md">
+          <h2 className="text-xl capitalize mb-4">"{thoughts[startIndex].shayari}"</h2>
+          <p className="text-[12px]">{thoughts[startIndex].des}</p>
         </div>
         <div className="flex items-center gap-5">
-          <div className="text-red-400 absolute z-50" onClick={handlePrev}>
+          {/* <div className="text-red-400 absolute z-50" onClick={handlePrev}>
           < FaRegArrowAltCircleLeft className="text-3xl" />
-          </div>
+          </div> */}
 
           {displayedImages.map((src, index) => (
-            <div key={index} className="w-64 h-80 carsoulProp rounded-md">
+            <div
+              key={index}
+              className="w-64 h-80 carsoulProp rounded-md"
+              onClick={handleNext}
+            >
               <img src={src} alt="" className="w-full h-full rounded-md" />
             </div>
           ))}
 
-          <button className="text-red-400 absolute right-0" onClick={handleNext}>
+          {/* <div className="text-red-400 absolute right-0" onClick={handleNext}>
             < FaRegArrowAltCircleRight className="text-3xl" />
-          </button>
+          </div> */}
         </div>
       </div>
       <audio ref={audioRef} autoPlay />
