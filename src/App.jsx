f2846ffe99d3144015a4ea43b902c46ab5365679
Deependra <deependra186037@gamil.com>
2024-06-15 00:40:10 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import "./App.css";
 import Carsoul from "./component/Carsoul";
-// import Player from "./component/SimplePlayer";
-import url1 from "./assets/Gulabi Sadi Ani Lali_320(PagalWorld.com.sb).mp3";
-import Player from "./component/song/Player";
 
 function App() {
   return (
@@ -10,7 +7,6 @@ function App() {
       <div>
         <div>
           <Carsoul/>
-          {/* <Player url={url1} /> */}
         </div>
       </div>
     </>
