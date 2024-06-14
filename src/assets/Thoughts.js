3db3c70fa8574537d36a083b6abc3d7a3691614c
Deependra <deependra186037@gamil.com>
2024-06-15 00:31:27 +0530

    almost done

---
+++ src/assets/Thoughts.js
@@ -0,0 +1,30 @@
+export const thoughts = [
+    {
+      shayari: "When I lost, I find you....",
+      des: "Do you have a map? Because I just keep getting lost in your eyes every time I see you. Your gaze pulls me in, leaving me breathless and enchanted....",
+    },
+    {
+      shayari: "In your eyes, I found my home....",
+      des: "If kisses were snowflakes, I'd send you a blizzard. Each kiss, a testament of my affection, would cover you, reminding you how deeply I cherish you....",
+    },
+    {
+      shayari: "Your smile is my sunshine....",
+      des: "I must be a snowflake, because I've fallen for you. Your warmth melts my heart, bringing joy and light into my life every single day....",
+    },
+    {
+      shayari: "Every moment with you is a treasure....",
+      des: "Are you a magician? Because whenever I look at you, everyone else disappears. Your presence is magical, making the world fade away as I focus on you....",
+    },
+    {
+      shayari: "Your love is my anchor....",
+      des: "If you were a vegetable, you'd be a cute-cumber. Your sweetness and charm captivate me, anchoring my heart in a sea of love and tenderness....",
+    },
+    {
+      shayari: "In your embrace, I find peace....",
+      des: "If you were a fruit, you'd be a fine-apple. Your embrace is a sanctuary where I find comfort and tranquility, rejuvenating my soul with your love....",
+    },
+    {
+      shayari: "Your presence is my comfort....",
+      des: "Do you have a Band-Aid? Because I just scraped my knee falling for you. Your presence heals my wounds, offering solace and warmth in times of need...",
+    },
+  ];
--- src/component/Carsoul.jsx
+++ src/component/Carsoul.jsx
@@ -6,6 +6,7 @@ import song3 from "./../assets/Maan Meri Jaan_320(PagalWorld.com.sb).mp3";
 import song4 from "./../assets/Teri Ada_320(PagalWorld.com.sb).mp3";
 import song5 from "./../assets/O Mahi O Mahi_320(PagalWorld.com.sb).mp3";
 import song6 from "./../assets/Oops_320(PagalWorld.com.sb).mp3";
+import { thoughts } from "./../assets/Thoughts";
 
 const images = [
   "https://images.unsplash.com/photo-1689602037070-fec2eca3f5b2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHx8",
@@ -16,66 +17,31 @@ const images = [
   "https://images.unsplash.com/photo-1718116088537-212b192d1ad9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D",
 ];
 
-const thoughts = [
-  {
-    shayari: "When I lost, I find you....",
-    des: "Do you have a map? Because I just keep getting lost in your eyes every time I see you. Your gaze pulls me in, leaving me breathless and enchanted....",
-  },
-  {
-    shayari: "In your eyes, I found my home....",
-    des: "If kisses were snowflakes, I'd send you a blizzard. Each kiss, a testament of my affection, would cover you, reminding you how deeply I cherish you....",
-  },
-  {
-    shayari: "Your smile is my sunshine....",
-    des: "I must be a snowflake, because I've fallen for you. Your warmth melts my heart, bringing joy and light into my life every single day....",
-  },
-  {
-    shayari: "Every moment with you is a treasure....",
-    des: "Are you a magician? Because whenever I look at you, everyone else disappears. Your presence is magical, making the world fade away as I focus on you....",
-  },
-  {
-    shayari: "Your love is my anchor....",
-    des: "If you were a vegetable, you'd be a cute-cumber. Your sweetness and charm captivate me, anchoring my heart in a sea of love and tenderness....",
-  },
-  {
-    shayari: "In your embrace, I find peace....",
-    des: "If you were a fruit, you'd be a fine-apple. Your embrace is a sanctuary where I find comfort and tranquility, rejuvenating my soul with your love....",
-  },
-  {
-    shayari: "Your presence is my comfort....",
-    des: "Do you have a Band-Aid? Because I just scraped my knee falling for you. Your presence heals my wounds, offering solace and warmth in times of need...",
-  },
-];
-
 const songs = [song1, song2, song3, song4, song5, song6];
 
 const Carousel = () => {
   const [startIndex, setStartIndex] = useState(0);
   const [audio, setAudio] = useState(new Audio(songs[0]));
-  const [playing, setPlaying] = useState(false);
-  const toggle = () => setPlaying(!playing);
+
   useEffect(() => {
-    audio.addEventListener("ended", () => setPlaying(false));
+    audio.play();
+    const handleAudioEnd = () =>
+      setStartIndex((prevIndex) => (prevIndex + 1) % songs.length);
+    audio.addEventListener("ended", handleAudioEnd);
+
     return () => {
-      audio.removeEventListener("ended", () => setPlaying(false));
+      audio.pause();
+      audio.removeEventListener("ended", handleAudioEnd);
     };
   }, [audio]);
 
   useEffect(() => {
-    if (playing) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  }, [playing, audio]);
-
-  useEffect(() => {
     audio.pause();
     const newAudio = new Audio(songs[startIndex]);
+    newAudio.play();
     setAudio(newAudio);
-    if (playing) {
-      newAudio.play();
-    }
+
+    return () => newAudio.pause(); // Ensure newAudio is cleaned up if it changes
   }, [startIndex]);
 
   const handleNext = () => {
@@ -113,7 +79,7 @@ const Carousel = () => {
         <div className="flex items-center gap-5">
           {displayedImages.map((src, index) => (
             <div
-              key={index}
+              key={startIndex + index} // Use unique keys
               className="w-64 h-80 carsoulProp rounded-md"
               onClick={handleNext}
             >
@@ -122,9 +88,6 @@ const Carousel = () => {
           ))}
         </div>
       </div>
-      <button className="z-50 bg-black text-white p-2 rounded" onClick={toggle}>
-        {playing ? "Pause" : "Play"}
-      </button>
     </div>
   );
 };
